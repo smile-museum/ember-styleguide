a29dc987e0d468aca24589d7aafef8886cdf6c1d
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-09-24 15:36:52 +0530

    Modify ember template linting rules

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
-  extends: 'recommended',
-  rules: {
-    'block-indentation': false,
-    'img-alt-attributes': false,
-    'no-unused-block-params': false,
-    'simple-unless': false,
-    quotes: 'double',
+  name: '@smile-io/ember-styleguide',
+  rules: {},
+  configurations: {
+    recommended: {
+      extends: 'recommended',
+      rules: {
+        'img-alt-attributes': false,
+        'simple-unless': false,
+        quotes: 'double'
+      },
+    },
   },
 };
